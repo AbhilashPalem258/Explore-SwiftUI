f10e94bace5a98f11b50a02f75ff5423d2226aa9
AbhilashShuttl <abhilash.palem@shuttl.com>
2022-06-29 11:01:41 +0530

    Initial commit

---
+++ Explore-SwiftUI/Explore-SwiftUI/Explore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_SwiftUIApp.swift
+//  Explore-SwiftUI
+//
+//  Created by Abhilash Palem on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Explore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
